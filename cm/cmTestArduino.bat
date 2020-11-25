3477b72fe9ee99513082b2c5841f1c821a78b1e2
EvanMateo7 <gevanmateo@gmail.com>
2020-11-25 18:08:56 -0500

    Update commands

--- cm/cmTestArduino.bat
+++ cm/cmTestArduino.bat
@@ -1,4 +1,4 @@
-avr-gcc -Os -Wall -DF_CPU=16000000UL -mmcu=atmega328p -D IORegToMockUart mainArduino.c _console.c bsl_COut.c bsl_xtoa.c hal.c ioreg.c out.c vm.c vmstack.c -o cmTestArduino.o
+avr-gcc -Os -Wall -DF_CPU=16000000UL -mmcu=atmega328p -D IORegToMockUart mainArduino.c bsl_console.c bsl_COut.c bsl_xtoa.c hal.c ioreg.c out.c vm.c vmstack.c -o cmTestArduino.o
 avr-objcopy -O ihex -j .text -j .data cmTestArduino.o  cmTestArduino.hex
 avrdude -c arduino -p atmega328p -b 57600 -P COM5 -D -Uflash:w:cmTestArduino.hex:i
 pause
