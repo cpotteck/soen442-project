9505b727c63ffef4a97390c281e24fc9dedc65aa
EvanMateo7 <gevanmateo@gmail.com>
2020-12-03 06:33:44 -0500

    Update commands to enums

--- cm/command_types.h
+++ cm/command_types.h
@@ -5,12 +5,14 @@
 #define ACK 0xCC
 #define NAK 0x33
 
-#define PING 0x20
-#define DOWNLOAD 0x21
-#define RUN 0x22
-#define GET_STATUS 0x23
-#define SEND_DATA 0x24
-#define RESET 0x25
+typedef enum {
+  PING = 0x20,
+  DOWNLOAD,
+  RUN,
+  GET_STATUS,
+  SEND_DATA,
+  RESET
+};
 
 struct ReadTargetBuffer {
   char* buffer;
